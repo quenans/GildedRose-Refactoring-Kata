461a446f79d798d6f1a44e5ebbbfaf985f6c115a
quenans <78828255+quenans@users.noreply.github.com>
2021-03-29 08:19:56 -0400

    ProjectCsci260

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -2,7 +2,7 @@ package com.gildedrose;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals; 
 
 class GildedRoseTest {
 
